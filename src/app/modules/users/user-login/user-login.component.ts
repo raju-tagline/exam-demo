7940d652a01d40446052f5467162b0f1e87f6286
Raju Tagline <raju.tagline@gmail.com>
2022-02-17 15:27:52 +0530

    animation add for user login success and error

--- src/app/modules/users/user-login/user-login.component.ts
+++ src/app/modules/users/user-login/user-login.component.ts
@@ -1,5 +1,6 @@
 import { UserDataService } from './../../../user-data.service';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user-login',
@@ -7,20 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-login.component.scss'],
 })
 export class UserLoginComponent implements OnInit {
-  constructor(private http: UserDataService) {}
+
+  constructor(
+    private http: UserDataService, 
+    private toastr: ToastrService
+    ) {}
 
   ngOnInit(): void {}
 
-  public onSubmit(event: any):void {
+  public onSubmit(event: any): void {
     const data = event.value;
     console.log('data :>> ', data);
 
     this.http.login(data).subscribe((res: any) => {
-      console.log('data :>> ', data);
-      console.log('res :>> ', res);
-      console.log('res.data :>> ', res.data);
-      alert(res.message);
+      if (res.statusCode === 200) {
+        this.toastr.success(res.message);
+      }
+      else{
+        this.toastr.error(res.message);
+      }
     });
-    console.log('submitted!!');
   }
 }
