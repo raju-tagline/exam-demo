2927059950eb060ffa7f870acb9e5f2f76027e73
Raju Tagline <raju.tagline@gmail.com>
2022-02-18 10:53:48 +0530

    with get api student data get and show successfully

--- src/app/modules/teacher/show-student-data/show-student-data.component.ts
+++ src/app/modules/teacher/show-student-data/show-student-data.component.ts
@@ -1,15 +1,20 @@
+import { StudentData, UserData } from './../../../interface';
+import { UserDataService } from './../../../user-data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-show-student-data',
   templateUrl: './show-student-data.component.html',
-  styleUrls: ['./show-student-data.component.scss']
+  styleUrls: ['./show-student-data.component.scss'],
 })
 export class ShowStudentDataComponent implements OnInit {
+  public studentData: StudentData[] = [ ];
 
-  constructor() { }
+  constructor(private userDataService: UserDataService) {}
 
   ngOnInit(): void {
+    this.userDataService.showStudentData().subscribe((data: any) => {
+      this.studentData = data.data;
+    });
   }
-
 }
