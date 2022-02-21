ffdc553a47a697e82259b3bad55c0ffc19f63942
Raju Tagline <raju.tagline@gmail.com>
2022-02-21 11:54:35 +0530

    student profile data show inton model and loading spinner added

--- src/app/modules/teacher/show-student-data/show-student-data.component.ts
+++ src/app/modules/teacher/show-student-data/show-student-data.component.ts
@@ -1,6 +1,8 @@
 import { StudentData, UserData } from './../../../interface';
 import { UserDataService } from './../../../user-data.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-show-student-data',
@@ -8,24 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-student-data.component.scss'],
 })
 export class ShowStudentDataComponent implements OnInit {
-  public studentData: StudentData[] = [ ];
-  public studentID!:String ;
+  public studentData: StudentData[] = [];
+  public studentID!: String;
+  public loadData: boolean = true;
 
-
-  constructor(private userDataService: UserDataService) {}
+  constructor(
+    private userDataService: UserDataService,
+    private router: Router,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.userDataService.showStudentData().subscribe((data: any) => {
       this.studentData = data.data;
+      this.loadData = false;
     });
   }
 
-  getId(id:any){
-    this.studentID = id;
-    console.log('id :>> ', this.studentID);
-    // this.userDataService.viewStudentData(id).subscribe((data: any) => {
-    //   this.studentID = data.data.name;
-    //   console.log('this.studentID :>> ', this.studentID);
-    // });
+  public studentProfileDetail(data: any): void {
+    this.modalService.open(data);
   }
 }
