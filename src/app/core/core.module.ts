39396aa5971a247c22aae760e39af2b660fd9780
Raju Tagline <raju.tagline@gmail.com>
2022-02-22 16:59:45 +0530

    new componet created for go back button into core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { BackComponent } from './back/back.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    BackComponent
   ],
   imports: [
     CommonModule
