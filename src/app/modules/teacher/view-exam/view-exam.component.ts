9df139c1c8bd33690806b69d35cabbb18a8077d7
Raju Tagline <raju.tagline@gmail.com>
2022-02-22 12:19:17 +0530

    exam details post api call and details show

--- src/app/modules/teacher/view-exam/view-exam.component.ts
+++ src/app/modules/teacher/view-exam/view-exam.component.ts
@@ -1,5 +1,7 @@
 import { UserDataService } from './../../../user-data.service';
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ExamDetailComponent } from '../exam-detail/exam-detail.component';
 
 @Component({
   selector: 'app-view-exam',
@@ -12,13 +14,22 @@ export class ViewExamComponent implements OnInit {
   ];
   public loadData:boolean = true;
 
-  constructor(private userDataService:UserDataService) { }
+  constructor(private userDataService:UserDataService,
+    private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.userDataService.viewExam().subscribe((res) => {
       this.examDetails = res.data;
       this.loadData = false
       console.log('res :>> ', res);
+    })
+  }
+
+  public viewExamDetails(id:any){
+    console.log('id :>> ', id);
+    this.userDataService.viewExamData(id).subscribe((res:any) => {
+      const modelRef = this.modalService.open(ExamDetailComponent);
+      modelRef.componentInstance.question = res?.data.questions;
     })
   }
   
