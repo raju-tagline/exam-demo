918f86fd43d62a0188e9615d93f17113b3ab98d9
Raju Tagline <raju.tagline@gmail.com>
2022-02-23 12:57:42 +0530

    show student profile api call and show data into component done

--- src/app/modules/student/student-profile/student-profile.component.ts
+++ src/app/modules/student/student-profile/student-profile.component.ts
@@ -1,15 +1,28 @@
+import { UserDataService } from './../../../user-data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student-profile',
   templateUrl: './student-profile.component.html',
-  styleUrls: ['./student-profile.component.scss']
+  styleUrls: ['./student-profile.component.scss'],
 })
 export class StudentProfileComponent implements OnInit {
+  public studentArr: any[] = [
+    // {
+    //   email: '',
+    //   name: '',
+    //   role: '',
+    //   _id: '',
+    // },
+  ];
+  public loadData: boolean = true;
 
-  constructor() { }
+  constructor(private userDataService: UserDataService) {}
 
   ngOnInit(): void {
+    this.userDataService.studentProfile().subscribe((res: any) => {
+      this.studentArr.push(res.data);
+      this.loadData = false;
+    });
   }
-
 }
