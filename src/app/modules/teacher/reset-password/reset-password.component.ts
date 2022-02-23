5678cb98f154faa522d7bf581a46de17cf0a1500
Raju Tagline <raju.tagline@gmail.com>
2022-02-23 12:44:35 +0530

    reset password for teacher module completed successfully

--- src/app/modules/teacher/reset-password/reset-password.component.ts
+++ src/app/modules/teacher/reset-password/reset-password.component.ts
@@ -1,23 +1,22 @@
 import { UserDataService } from './../../../user-data.service';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent implements OnInit {
+  constructor(private userDataService: UserDataService,private toastr:ToastrService) {}
 
-  constructor(private userDataService:UserDataService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  public reset(event:any):void{
-    console.log('event :>> ', event);
-    this.userDataService.resetPassword(event).subscribe((res) => {
+  public reset(event: any): void {
+    const data:any = {email:event.value};
+    this.userDataService.resetPassword(data).subscribe((res)=>{
+      this.toastr.success(res?.message);
       console.log('res :>> ', res);
-    });
+    })
   }
-
 }
