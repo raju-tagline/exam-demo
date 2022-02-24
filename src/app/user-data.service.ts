43049b66e7470eea926d71fa1cdc46198e0e2b75
Raju Tagline <raju.tagline@gmail.com>
2022-02-24 15:54:18 +0530

    update url path for api call

--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -32,17 +32,17 @@ export class UserDataService implements OnInit {
   ngOnInit(): void {}
 
   public signUp(data: IUserData): Observable<IUserDataResponse> {
-    return this.http.post<IUserDataResponse>(this.url + 'users/SignUp', data);
+    return this.http.post<IUserDataResponse>(`${this.url}users/SignUp`, data);
   }
 
   public login(login: IUserData): Observable<IUserDataResponse> {
-    return this.http.post<IUserDataResponse>(this.url + 'users/Login', login);
+    return this.http.post<IUserDataResponse>(`${this.url}users/Login`, login);
   }
 
   public showStudentData(): Observable<IStudentDataResponse> {
     const headers = new HttpHeaders().set('access-token', this.token);
     return this.http.get<IStudentDataResponse>(
-      this.url + 'dashboard/Teachers',
+      `${this.url}dashboard/Teachers`,
       {
         headers: headers,
       }
@@ -54,7 +54,7 @@ export class UserDataService implements OnInit {
   ): Observable<IVerifyStudentDataResponse> {
     const headers = new HttpHeaders().set('access-token', this.token);
     const getUrl =
-      this.url + 'dashboard/Teachers/viewStudentDetail?id=' + userID;
+      `${this.url}dashboard/Teachers/viewStudentDetail?id=` + userID;
     return this.http.get<IVerifyStudentDataResponse>(getUrl, {
       headers: headers,
     });
@@ -63,7 +63,7 @@ export class UserDataService implements OnInit {
   public verifiedStudentData(): Observable<IShowStudentProfileResponse> {
     const headers = new HttpHeaders().set('access-token', this.token);
     return this.http.get<IShowStudentProfileResponse>(
-      this.url + 'dashboard/Teachers/StudentForExam',
+      `${this.url}dashboard/Teachers/StudentForExam`,
       {
         headers: headers,
       }
@@ -73,7 +73,7 @@ export class UserDataService implements OnInit {
   public viewExam(): Observable<IViewExamResponse> {
     const headers = new HttpHeaders().set('access-token', this.token);
     return this.http.get<IViewExamResponse>(
-      this.url + 'dashboard/Teachers/viewExam',
+      `${this.url}dashboard/Teachers/viewExam`,
       {
         headers: headers,
       }
@@ -82,7 +82,7 @@ export class UserDataService implements OnInit {
 
   public viewExamData(examId: string): Observable<IViewStudentExamResponse> {
     const headers = new HttpHeaders().set('access-token', this.token);
-    const getUrl = this.url + 'dashboard/Teachers/examDetail?id=' + examId;
+    const getUrl = `${this.url}dashboard/Teachers/examDetail?id=` + examId;
     return this.http.get<IViewStudentExamResponse>(getUrl, {
       headers: headers,
     });
@@ -91,7 +91,7 @@ export class UserDataService implements OnInit {
   public studentDetail(): Observable<IStudentProfileResponse> {
     const headers = new HttpHeaders().set('access-token', this.token);
     return this.http.get<IStudentProfileResponse>(
-      this.url + 'student/getStudentDetail',
+      `${this.url}student/getStudentDetail`,
       {
         headers: headers,
       }
@@ -101,7 +101,7 @@ export class UserDataService implements OnInit {
   public viewExamPaper(userId: string): Observable<IUserData> {
     const headers = new HttpHeaders().set('access-token', this.token);
     return this.http.get<IUserData>(
-      this.url + 'student/examPaper?id=' + userId,
+      `${this.url}student/examPaper?id=` + userId,
       {
         headers: headers,
       }
@@ -120,7 +120,7 @@ export class UserDataService implements OnInit {
   public viewStudentAllExam(): Observable<IStudentExamResponse> {
     const headers = new HttpHeaders().set('access-token', this.token);
     return this.http.get<IStudentExamResponse>(
-      this.url + 'student/studentExam',
+      `${this.url}student/studentExam`,
       {
         headers: headers,
       }
@@ -130,7 +130,7 @@ export class UserDataService implements OnInit {
   public studentProfile(): Observable<IStudentProfileResponse> {
     const headers = new HttpHeaders().set('access-token', this.token);
     return this.http.get<IStudentProfileResponse>(
-      this.url + 'student/getStudentDetail',
+      `${this.url}student/getStudentDetail`,
       {
         headers: headers,
       }
@@ -148,7 +148,7 @@ export class UserDataService implements OnInit {
   }
 
   public updateStudentProfile() {
-    return this.http.put(this.url + 'student/studentProfile', this.token);
+    return this.http.put(`${this.url}` + 'student/studentProfile', this.token);
   }
 
   public LogOut(): void {
