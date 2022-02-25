bdbcc9fab8ddcc8b78e512dbf8a11b54e07f20f7
Raju Tagline <raju.tagline@gmail.com>
2022-02-25 09:24:47 +0530

    created new core component for reset user password

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BackComponent } from './back/back.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    BackComponent
+    BackComponent,
+    ResetPasswordComponent
   ],
   imports: [
     CommonModule,
