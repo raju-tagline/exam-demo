5225d36db3c4a4874dd77584ac37fef717b5be7e
Raju Tagline <raju.tagline@gmail.com>
2022-03-17 10:15:28 +0530

    login form spec testing with cypress done

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,9 +1,67 @@
-describe('First login test', () => {
-  xit('Incorrect password for user login', () => {
-    cy.login('raju.tagline+1@gmail.com', 'raju1234');
+describe('exxam demo login spec', () => {
+  xit('Check validation of email by touched and untouched', () => {
+    cy.visit('/');
+    cy.url().should('include', 'login');
+    cy.get('#email').click();
+    cy.get('#form-div').click('bottom');
+    cy.wait(3000);
   });
 
-  it('Visits the initial project page first time', () => {
-    cy.login('raju.tagline+1@gmail.com','Raju1234')
+  xit('Check validation of password by touched and untouched', () => {
+    cy.visit('/');
+    cy.url().should('include', 'login');
+    cy.get('#password').click();
+    cy.get('#form-div').click('bottom');
+    cy.wait(3000);
+  });
+
+  xit('check email validation should have @ and proper valid mail', () => {
+    cy.visit('/');
+    cy.url().should('include', 'login');
+    cy.get('#email').type('asdfasdf');
+    cy.get('#btn-login').click();
+    // cy.get('#form-div').click('bottom');
+    cy.wait(3000);
+  });
+
+  xit('check email validation with @@ at same time', () => {
+    cy.visit('/');
+    cy.url().should('include', 'login');
+    cy.get('#email').type('asdfasdf@@');
+    cy.get('#btn-login').click();
+    // cy.get('#form-div').click('bottom');
+    cy.wait(3000);
+  });
+
+  xit('check email validation with . at last of email', () => {
+    cy.visit('/');
+    cy.url().should('include', 'login');
+    cy.get('#email').type('asdfasdf@gmail.');
+    cy.get('#btn-login').click();
+    cy.wait(3000);
+  });
+
+  xit('check password validation should have proper length', () => {
+    cy.visit('/');
+    cy.url().should('include', 'login');
+    cy.get('#password').type('sa');
+    cy.get('#btn-login').click();
+    cy.wait(3000);
+  });
+
+  xit('check both email and password area valid or not', () => {
+    cy.visit('/');
+    cy.url().should('include', 'login');
+    cy.get('#email').type('raju.tagline+23@gmail..com');
+    cy.get('#password').type('raju1234');
+    cy.get('#btn-login').click();
+  });
+
+  xit('check auth service is used or not', () => {
+    cy.visit('/teacher');
+  })
+
+  xit('Visits the initial project page first time', () => {
+    cy.login('raju.tagline+1@gmail.com', 'Raju1234');
   });
 });
