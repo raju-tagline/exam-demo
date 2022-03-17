0657d650c9653e5c41bb5cfd7a3d482909018014
Raju Tagline <raju.tagline@gmail.com>
2022-03-17 12:39:24 +0530

    for sing up spec new function define singup

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,23 +3,61 @@
 // with Intellisense and code completion in your
 // IDE or Text Editor.
 // ***********************************************
+
+// -------DECLARE TYPE INTERFACE OF DECLARE FUNCTION KEYWORD-------
 declare namespace Cypress {
   interface Chainable<Subject = any> {
     login(email: string, password: string): typeof login;
   }
+  interface Chainable<Subject = any> {
+    singup(
+      name: string,
+      email: string,
+      password: string,
+      role: string
+    ): typeof singup;
+  }
+  interface Chainable<Subject = any> {
+    getSignUp(): typeof getSignUp;
+  }
 }
 
+// -------LOGIN FUNCTION-------
 function login(email: string, password: string): void {
   cy.visit('/');
   cy.url().should('include', 'login');
   cy.get('#email').type(email);
   cy.get('#password').type(password);
   cy.get('#btn-login').click();
-//   console.warn(email, password);
 }
 
-// NOTE: You can use it like so:
 Cypress.Commands.add('login', login);
+
+// -------SIGN UP FUNCTION-------
+function singup(
+  name: string,
+  email: string,
+  password: string,
+  role: string
+): void {
+  cy.getSignUp();
+  cy.get('[ formControlName="name" ]').type(name);
+  cy.get('[formControlName = "email"]').type(email);
+  cy.get('[formControlName = "password"]').type(password);
+  cy.get('[ formControlName ="role" ]').select(role);
+  //cy.get('button').click();
+}
+
+Cypress.Commands.add('singup', singup);
+
+// GET SINF UP
+function getSignUp() {
+  cy.visit('/');
+  cy.get('#a-singup').click();
+  cy.url().should('include', 'signup');
+}
+Cypress.Commands.add('getSignUp', getSignUp);
+
 //
 // ***********************************************
 // This example commands.js shows you how to
