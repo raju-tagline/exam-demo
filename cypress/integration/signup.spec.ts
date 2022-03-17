ea17bb791707538157d5b25c52fac75e661555ff
Raju Tagline <raju.tagline@gmail.com>
2022-03-17 12:40:35 +0530

    singup spec testing using touch,untouch,valid input done

--- cypress/integration/signup.spec.ts
+++ cypress/integration/signup.spec.ts
@@ -1,12 +1,41 @@
 describe('First signup test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/');
-    cy.get('#a-singup').click();
-    cy.url().should('include', 'signup');
-    cy.get('[ formControlName="name" ]').type('test one');
-    cy.get('[formControlName = "email"]').type('cypress.test.raju@gmail.com');
-    cy.get('[formControlName = "password"]').type('Raju1234');
-    cy.get('[ formControlName ="role" ]').select('Teacher');
-    //cy.get('button').click();
+  xit('check validation in user sign up without and details', () => {
+    cy.getSignUp();
+    cy.get('#btn-signup').click();
+  });
+
+  xit('check validation of name using touch and untouch', () => {
+    cy.getSignUp();
+    cy.get('#name').click();
+    cy.get('#form-div').click('bottom');
+  });
+
+  xit('check validation of email using touch and untouch', () => {
+    cy.getSignUp();
+    cy.get('#email').click();
+    cy.get('#form-div').click('bottom');
+  });
+
+  xit('check validation of password using touch and untouch', () => {
+    cy.getSignUp();
+    cy.get('#password').click();
+    cy.get('#form-div').click('bottom');
+  });
+
+  xit('check email validation using pattern', () => {
+    cy.getSignUp();
+    cy.get('#email').type('asdfasdf');
+    cy.get('#form-div').click('bottom');
+  });
+
+  it('check email validation using pattern', () => {
+    cy.getSignUp();
+    cy.get('#email').type('asdfasdf@asdf');
+    cy.get('#form-div').click('bottom');
+  });
+
+  xit('Visits the initial project page', () => {
+    cy.singup('test-1', 'test@test.com', 'test1234', 'Teacher');
+    cy.get('button').click();
   });
 });
